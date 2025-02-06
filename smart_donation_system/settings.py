3dc7791bb68069a3bb30b348b598bc5153022d69
yosrzohair <yosirdirani@gmail.com>
2025-02-06 14:54:36 +0300

    updates on  apps and urls

--- smart_donation_system/settings.py
+++ smart_donation_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+-e%bdj&twl!3=)w2y5y84_ocj_hazk58(oxuao#*9&0_hlzy%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
+    "notifications",
+    "inventory",
+    "donations",
+    "dashboard",
 ]
 
 MIDDLEWARE = [
--- smart_donation_system/urls.py
+++ smart_donation_system/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("dashboard/", include('dashboard.urls')),
+    path("donations/", include('donations.urls')),
+    path("users/", include('users.urls')),
+    path("inventory/", include('inventory.urls')),
+    path("notifications/", include('notifications.urls')),
 ]
